672cec6a2bf924cc29dee92f8879119251feeb08
Uladzislau Khamkou <kham@tuta.io>
2022-02-15 23:47:19 +0300

    support "from ... import ... as ..."

--- linker.py
+++ linker.py
@@ -24,10 +24,28 @@ def parse_import(module):
     return import_to_def(module, path) + [f"{module} = {module}()"]
 
 
+def parse_from(module, vrbls):
+    path = Path(".") / f"{module}.py"
+    if not path.is_file():
+        vrbls = ", ".join("{m} as {l}" if m != l else m for m, l in vrbls)
+        return [f"from {module} import {vrbls}"]
+    mns = f"{module}_namespace"
+    src = import_to_def(mns, path)
+    src.append(f"{mns} = {mns}()")
+    for module_name, local_name in vrbls:
+        src.append(f"{local_name} = {mns}.{module_name}")
+    return src
+
+
 def link(source):
     for line in source.split("\n"):
         if line.startswith("import "):
             yield from parse_import(line.split(" ", 1)[1])
+        elif line.startswith("from "):
+            _, module, _, vrbles = line.split(" ", 3)
+            vrbles = map(lambda vrbl: vrbl.split(" as "), vrbles.split(", "))
+            vrbles = [v if len(v) == 2 else [v[0], v[0]] for v in vrbles]
+            yield from parse_from(module, vrbles)
         else:
             yield line
 
