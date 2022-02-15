bebb2fef7c329e170763cf3588b9a013f161631c
Uladzislau Khamkou <kham@tuta.io>
2022-02-16 01:17:49 +0300

    support cyclic dependencies

--- linker.py
+++ linker.py
@@ -7,51 +7,103 @@ def indent_line(code, level=1):
     return ""
 
 
-def build_module_definition(module, src_path):
-    src = [f"def {module}():"]
-    src += list(map(indent_line, src_path.read_text().strip().split("\n")))
-    return src + """
-    local_module_namespace = __import__("types").SimpleNamespace()
-    for variable, value in list(locals().items()):
-        setattr(local_module_namespace, variable, value)
-    return local_module_namespace""".split("\n")
+def is_user_built_module(module_name):
+    path = Path(".") / f"{module_name}.py"
+    return path.is_file()
 
 
-def parse_import(module):
-    path = Path(".") / f"{module}.py"
-    if not path.is_file():
-        return [f"import {module}"]
-    return build_module_definition(module, path) + [f"{module} = {module}()"]
-
-
-def parse_from(module, vrbls):
-    path = Path(".") / f"{module}.py"
-    if not path.is_file():
-        vrbls = ", ".join("{m} as {l}" if m != l else m for m, l in vrbls)
-        return [f"from {module} import {vrbls}"]
-    mns = f"{module}_namespace"
-    src = build_module_definition(mns, path)
-    src.append(f"{mns} = {mns}()")
-    for module_name, local_name in vrbls:
-        src.append(f"{local_name} = {mns}.{module_name}")
-    return src
-
-
-def link(source):
-    for line in source.split("\n"):
+def parse_imports(source):
+    source = source.split("\n") if isinstance(source, str) else source
+    imports = set()
+    for line in source:
         if line.startswith("import "):
-            yield from parse_import(line.split(" ", 1)[1])
+            imports.add((line.split(" ", 1)[1], None))
+        elif line.startswith("from "):
+            _, module, _, vrbles = line.split(" ", 3)
+            vrbles = map(lambda vrbl: vrbl.split(" as "), vrbles.split(", "))
+            vrbles = tuple(v if len(v) == 2 else (v[0], v[0]) for v in vrbles)
+            imports.add((module, tuple(map(tuple, vrbles))))
+    return imports
+
+
+def parse_deps(source):
+    stack, deps, visited = [m for m, _ in parse_imports(source)], set(), set()
+    while stack:
+        imprt = stack.pop()
+        if not is_user_built_module(imprt):
+            continue
+        deps.add(imprt)
+        for m, _ in parse_imports(Path(imprt + ".py").read_text()):
+            if m not in visited:
+                visited.add(m)
+                stack.append(m)
+    return deps
+
+
+def refactor_imports(source, deps):
+    source = source.split("\n") if isinstance(source, str) else source
+    for line in source:
+        if line.startswith("import "):
+            module = line.split(" ", 1)[1]
+            if module in deps:
+                yield f"{module} = {module}_namespace()"
+                continue
         elif line.startswith("from "):
             _, module, _, vrbles = line.split(" ", 3)
             vrbles = map(lambda vrbl: vrbl.split(" as "), vrbles.split(", "))
             vrbles = [v if len(v) == 2 else [v[0], v[0]] for v in vrbles]
-            yield from parse_from(module, vrbles)
-        else:
-            yield line
+            if module in deps:
+                for local_vrbl, vrbl in vrbles:
+                    yield f"{vrbl} = {module}_namespace().{local_vrbl}"
+                continue
+        yield line
+
+
+class Module:
+    def __init__(self, name, source, imports, deps):
+        self.name = name
+        self.source = source
+        self.imports = imports
+        self.deps = deps - {name}
+
+    @staticmethod
+    def from_source(path, source=None):
+        path = Path(path)
+        name = path.stem
+        source = source if source else path.read_text()
+        source = source.split("\n") if isinstance(source, str) else source
+        return Module(name, source, parse_imports(source), parse_deps(source))
+
+    def to_namespace(self):
+        mns = f"{self.name}_namespace"
+        code = refactor_imports(self.source, self.deps)
+        src = [f"def {mns}():"] + list(map(indent_line, code))
+        return src + f"""    if True:
+        {mns}_content = list(locals().items())
+        global {mns}
+        {mns} = CallableSimpleNamespace()
+        for variable, value in {mns}_content:
+            setattr({mns}, variable, value)
+        return {mns}""".split("\n")
+
+    def build(self):
+        c = "CallableSimpleNamespace"
+        code = [f'class {c}(__import__("types").SimpleNamespace):']
+        code.append(indent_line("__call__ = lambda self: self"))
+        code += ['']*2
+        for dep in self.deps:
+            code += Module.from_source(dep + ".py").to_namespace()
+            code += ['']*2
+        return code + list(refactor_imports(self.source, self.deps))
+
+    def __repr__(self):
+        name, source = self.name, self.source
+        imports, deps = self.imports, self.deps
+        return f"Module({name}, {source}, {imports}, {deps})"
 
 
 if __name__ == "__main__":
     import sys
 
-    src, trgt = sys.argv[1], sys.argv[2]
-    Path(trgt).write_text("\n".join(link(Path(src).read_text())))
+    source, target = sys.argv[1], sys.argv[2]
+    Path(target).write_text("\n".join(Module.from_source(source).build()))
